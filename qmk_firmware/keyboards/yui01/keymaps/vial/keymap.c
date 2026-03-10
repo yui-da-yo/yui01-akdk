c6e783ecebab00e83c240a562a93151edde78859
yui-da-yo <yuitarebuu_1124c@yahoo.co.jp>
2026-03-10 20:42:45 +0900

    Update keymap.c
    レイヤー数を6にするためコードを修正。

--- qmk_firmware/keyboards/yui01/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/yui01/keymaps/vial/keymap.c
@@ -1,2 +1,16 @@
 #include QMK_KEYBOARD_H
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // Layer 0
+    [0] = LAYOUT(LAYOUT_Vial_Standard),
+    // Layer 1
+    [1] = LAYOUT(LAYOUT_Vial_Standard),
+    // Layer 2
+    [2] = LAYOUT(LAYOUT_Vial_Standard),
+    // Layer 3
+    [3] = LAYOUT(LAYOUT_Vial_Standard),
+    // Layer 4
+    [4] = LAYOUT(LAYOUT_Vial_Standard),
+    // Layer 5
+    [5] = LAYOUT(LAYOUT_Vial_Standard)
+};
