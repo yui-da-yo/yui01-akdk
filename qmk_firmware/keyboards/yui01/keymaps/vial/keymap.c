5b605c13ea7ac572a622a33212fef2db4f2dc3a1
yui-da-yo <yuitarebuu_1124c@yahoo.co.jp>
2026-03-11 23:52:23 +0900

    Update keymap.c

--- qmk_firmware/keyboards/yui01/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/yui01/keymaps/vial/keymap.c
@@ -7,9 +7,6 @@
 #define _L4 4
 #define _L5 5
 
-// --- 追加: スクロールモード管理用の変数 ---
-static bool is_scroll_mode = false;
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_L0] = LAYOUT(
         KC_X,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -54,38 +51,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS
     )
 };
-
 // マウスの動きを検知してVialのオートマウス機能に伝える処理
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    // --- 追加: スクロールモードがONの場合、動きをホイール回転に変換 ---
-    if (is_scroll_mode) {
-        mouse_report.h = mouse_report.x; // 左右
-        mouse_report.v = -mouse_report.y; // 上下（反転）
-        mouse_report.x = 0;
-        mouse_report.y = 0;
-    }
-
-    // 元々のオートマウス処理（変更なし）
+    // x軸またはy軸に動きがあった場合
     if (mouse_report.x != 0 || mouse_report.y != 0) {
+        // オートマウス機能を有効化し、タイマーをリセットする
         set_auto_mouse_enable(true);
     }
     return mouse_report;
 }
 #endif
-
 void pointing_device_init_user(void) {
+    // 起動時に感度を強制的に上書き
     pointing_device_set_cpi(500);
-}
-
-// --- 追加: User 0キーでスクロールモードを切り替える処理 ---
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case VIAL_USER00: // Vialの「User 0」に相当
-            if (record->event.pressed) {
-                is_scroll_mode = !is_scroll_mode; // 押すたびにON/OFF切り替え
-            }
-            return false;
-    }
-    return true;
 }
