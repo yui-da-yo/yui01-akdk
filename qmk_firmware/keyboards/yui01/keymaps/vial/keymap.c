72095722c8c8321ef0f9386e0b2188ca96ad6cfb
yui-da-yo <yuitarebuu_1124c@yahoo.co.jp>
2026-03-12 01:52:35 +0900

    Update keymap.c
    オートマウスレイヤまで実現。スクロールモードは無し。

--- qmk_firmware/keyboards/yui01/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/yui01/keymaps/vial/keymap.c
@@ -9,14 +9,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_L0] = LAYOUT(
-        KC_B,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_X,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS
     ),
     [_L1] = LAYOUT(
-        KC_C, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_Y, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -51,40 +51,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS
     )
 };
-
-// --- ここからが機能の心臓部 ---
-
-static bool is_scrolling = false; // スクロール中かどうかのスイッチ
-
-// 1. スクロールモードの切り替え
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode == 0x5F80) { // User 0
-        is_scrolling = record->event.pressed;
-        return false;
-    }
-    return true;
-}
-
-// 2. ドライバの上書きを防ぐ「最終処理」でスクロールを実現
-report_mouse_t pointing_device_task_combined_user(report_mouse_t mouse_report) {
-    // 修正ポイント：データを消す前に、移動（x, y）があるか判定してオートマウスを維持する
+// マウスの動きを検知してVialのオートマウス機能に伝える処理
+#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    // x軸またはy軸に動きがあった場合
     if (mouse_report.x != 0 || mouse_report.y != 0) {
+        // オートマウス機能を有効化し、タイマーをリセットする
         set_auto_mouse_enable(true);
     }
-
-    if (is_scrolling) {
-        // スクロール中なら、移動量をスクロール量(v, h)に変換する
-        mouse_report.v = -mouse_report.y; 
-        mouse_report.h = mouse_report.x;
-        // カーソル移動をリセット
-        mouse_report.x = 0;
-        mouse_report.y = 0;
-    }
-
     return mouse_report;
 }
-
-// 3. 感度設定
+#endif
 void pointing_device_init_user(void) {
+    // 起動時に感度を強制的に上書き
     pointing_device_set_cpi(500);
 }
