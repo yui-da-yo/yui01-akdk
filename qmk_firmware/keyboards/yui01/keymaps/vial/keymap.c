1c630b5a74911909d518d5385f76d0997510b283
yui-da-yo <yuitarebuu_1124c@yahoo.co.jp>
2026-03-12 12:19:03 +0900

    Update keymap.c

--- qmk_firmware/keyboards/yui01/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/yui01/keymaps/vial/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-// 状態管理
 static bool is_drag_scroll = false;
 static uint16_t mouse_timer = 0;
 
@@ -8,63 +7,34 @@ enum custom_keycodes {
     DRAG_SCROLL = SAFE_RANGE,
 };
 
-// レイヤー定義
-#define L_BASE 0
-#define L_MOUSE 1 
+// ... レイヤー定義（省略せず、あなたの現在の keymap.c のものをそのまま使ってください） ...
 
-#define _L0 0
-#define _L1 1
-#define _L2 2
-#define _L3 3
-#define _L4 4
-#define _L5 5
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_L0] = LAYOUT(
-        KC_A,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS
-    ),
-    [_L1] = LAYOUT(
-        KC_B, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS
-    ),
-    [_L2] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    [_L3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    [_L4] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    [_L5] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
-};
-
-// OS送信直前のフック
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    
-    // 【実験1のメイン：強制ゼロ上書き】
-    // この2行が効いていれば、マウスカーソルは一切動かなくなります。
-    mouse_report.x = 0;
-    mouse_report.y = 0;
+    // 判定用に元の値を保持
+    int8_t raw_x = mouse_report.x;
+    int8_t raw_y = mouse_report.y;
 
-    // オートマウスの判定（動き自体はmouse_reportに残っているので反応するはず）
-    if (mouse_report.x != 0 || mouse_report.y != 0) {
-        if (!IS_LAYER_ON(L_MOUSE)) {
-            layer_on(L_MOUSE);
+    // 【ここが本番】ドラッグスクロールが ON なら座標をスクロールへ転送
+    if (is_drag_scroll) {
+        mouse_report.h = mouse_report.x;
+        mouse_report.v = mouse_report.y;
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+    }
+
+    // オートマウス判定（生の値 raw_x/y を使う）
+    if (raw_x != 0 || raw_y != 0) {
+        if (!IS_LAYER_ON(1)) { // マウスレイヤーを1と仮定
+            layer_on(1);
         }
         mouse_timer = timer_read();
     } else {
-        if (IS_LAYER_ON(L_MOUSE) && timer_elapsed(mouse_timer) > 500) {
-            layer_off(L_MOUSE);
+        if (IS_LAYER_ON(1) && timer_elapsed(mouse_timer) > 500) {
+            layer_off(1);
         }
     }
 
     return mouse_report;
-}
-
-void pointing_device_init_user(void) {
-    pointing_device_set_cpi(500);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
