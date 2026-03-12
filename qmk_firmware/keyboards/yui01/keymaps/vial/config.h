047f18fffdf837461cc359e6f41b89c05c453ef2
yui-da-yo <yuitarebuu_1124c@yahoo.co.jp>
2026-03-12 15:24:27 +0900

    Update config.h
    コード内容を整理。

--- qmk_firmware/keyboards/yui01/keymaps/vial/config.h
+++ qmk_firmware/keyboards/yui01/keymaps/vial/config.h
@@ -1,31 +1,46 @@
 #pragma once
 
+// （１）基本設定
+// 左右の判定をEEPROM（基板のメモリ）で行う
 #define EE_HANDS
+// VIAL識別用の固有ID
 #define VIAL_KEYBOARD_UID {0xc4, 0xfd, 0xa3, 0x5f, 0x87, 0xa9, 0x48, 0xca}
-#define VIAL_TAP_DANCE_ENTRIES 8
-#define VIAL_COMBO_ENTRIES 8
+// タップダンス機能の最大登録数
+#define VIAL_TAP_DANCE_ENTRIES 10
+// コンボ機能の最大登録数
+#define VIAL_COMBO_ENTRIES 10
+// レイヤー数6（0～5レイヤー）
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+// RP2040等のフラッシュメモリ容量指定
 #define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
 
+// （２）トラックボール関連設定
+// 分割キーボード間でのポインティングデバイス通信を有効化
 #define SPLIT_POINTING_ENABLE
+// トラックボールが左手側に配置されていることを定義
 #define POINTING_DEVICE_LEFT
+// X軸（横方向）の移動を反転（左手トラボのため）
 #define POINTING_DEVICE_INVERT_X
+// Y軸（縦方向）の移動を反転（左手トラボのため）
 #define POINTING_DEVICE_INVERT_Y
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
-
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define VIAL_AUTO_MOUSE_LAYER 1
-// 最小の動き（1カウント）で反応させる設定を追加
-#define VIAL_AUTO_MOUSE_THRESHOLD 1 
-// 猶予時間を1秒（1000ms）に延長
-#define VIAL_AUTO_MOUSE_TIMEOUT 1000 
-#define VIAL_AUTO_MOUSE_ON_BY_DEFAULT
-
+// マウスカーソルの移動速度（数値が大きいほど速い）
 #define POINTING_DEVICE_CPI 400
 
-/* --- 修正箇所：ここから --- */
-// ドラッグスクロール機能を有効化
-#define POINTING_DEVICE_DRAG_SCROLL_ENABLE
+// （３）オートマウスレイヤ関連設定
+// オートマウスレイヤー機能を有効化
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+// マウス操作時に自動で切り替わるレイヤー番号
+#define VIAL_AUTO_MOUSE_LAYER 1
+// 反応感度。1カウントの小さな動きでもレイヤーを切り替える
+#define VIAL_AUTO_MOUSE_THRESHOLD 1 
+// マウス停止後、元のレイヤーに戻るまでの猶予時間(ms)
+#define VIAL_AUTO_MOUSE_TIMEOUT 1000 
+// キーボード起動時にオートマウス機能をONにする
+#define VIAL_AUTO_MOUSE_ON_BY_DEFAULT
 
-// ボタンを押している間だけスクロールにする設定を追加
+// （４）スクロール機能関連設定
+// QMK標準のドラッグスクロール機能を有効化（keymap.cでのカスタム制御と併用）
+#define POINTING_DEVICE_DRAG_SCROLL_ENABLE
+// ボタン（またはレイヤー）がアクティブな間だけスクロールモードを維持する
 #define POINTING_DEVICE_DRAG_SCROLL_MOMENTARY
-/* --- 修正箇所：ここまで --- */
+
