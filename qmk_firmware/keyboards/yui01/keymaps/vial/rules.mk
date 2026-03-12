4a55e4d35206da2176594cfc0b3ce8916d4ba6fc
yui-da-yo <yuitarebuu_1124c@yahoo.co.jp>
2026-03-12 16:21:07 +0900

    Update rules.mk
    コードを整理し解説を補足

--- qmk_firmware/keyboards/yui01/keymaps/vial/rules.mk
+++ qmk_firmware/keyboards/yui01/keymaps/vial/rules.mk
@@ -1,15 +1,23 @@
+# オートマウス機能を有効化
 POINTING_DEVICE_AUTO_MOUSE_ENABLE = yes
-
+# VIAL上でのオートマウス設定を有効化
 VIAL_AUTO_MOUSE_ENABLE = yes
+# VIALのレイヤー切り替え機能を有効化
 VIAL_AUTO_MOUSE_LAYER_ENABLE = yes
 
-# この下の行の「#」を削除しました
+# マウスキー機能（キー入力でのマウス操作）を有効化
 MOUSEKEY_ENABLE = yes
 
+# VIA設定機能を有効化
 VIA_ENABLE = yes
+# VIAL設定機能を有効化
 VIAL_ENABLE = yes
+# VIALのセキュリティロックを無効化（設定保存を容易にする）
 VIAL_INSECURE = yes
 
-POINTING_DEVICE_DRIVER = custom
+# ポインティングデバイス機能を全体的に有効化
 POINTING_DEVICE_ENABLE = yes
+# ポインティングデバイスのドライバをカスタム設定に指定
+POINTING_DEVICE_DRIVER = custom
+# 使用するセンサー（paw3222）のソースファイルを追加
 SRC += paw3222.c
